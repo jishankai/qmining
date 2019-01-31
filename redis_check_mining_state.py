f03695086ff78feb8a1c7b9f0cc5d355c042fa89
Jie You <jieyou@quarkchain.org>
2019-01-30 16:16:13 -0800

    remote visit redis-cli
    python3 redis_check_mining_state.py --ip 54.202.245.214 --port 6379

--- redis_check_mining_state.py
+++ redis_check_mining_state.py
@@ -3,7 +3,7 @@ import argparse
 
 def query_state(redis_ip, redis_port):
 
-    r = redis.StrictRedis(host='localhost', port=redis_port, db=0)
+    r = redis.StrictRedis(host==redis_ip, port=redis_port, db=0)
     for key in r.scan_iter("eth:miners:*"):
         print(key, r.hgetall(key))
 
